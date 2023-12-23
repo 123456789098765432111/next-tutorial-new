40829d0359618a748e66b1f35cbee505a0f42620
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-23 12:20:08 +0500

    Update route.js

--- src/app/api/config/route.js
+++ src/app/api/config/route.js
@@ -1,16 +1,16 @@
-import { NextResponse,NextRequest } from "next/server";
+// import { NextResponse,NextRequest } from "next/server";
 
-// pages/api/config.js
+// // pages/api/config.js
 
-export const GET = async () => {
-  if (NextRequest.method === 'GET') {
-    try {
-      const email = process.env.email || 'default@email.com';
-      NextResponse.json({ email },{status:200});
-    } catch (error) {
-      NextResponse.json({ error: 'Internal Server Error' },{status:500});
-    }
-  } else {
-    NextResponse.json({ message:"Not Allowed" },{status:403}); // Return method not allowed for other methods
-  }
-};
+// export const GET = async () => {
+//   if (NextRequest.method === 'GET') {
+//     try {
+//       const email = process.env.email || 'default@email.com';
+//       NextResponse.json({ email },{status:200});
+//     } catch (error) {
+//       NextResponse.json({ error: 'Internal Server Error' },{status:500});
+//     }
+//   } else {
+//     NextResponse.json({ message:"Not Allowed" },{status:403}); // Return method not allowed for other methods
+//   }
+// };
