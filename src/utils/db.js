cf3b2b06c4d4f38e066e543fecb385e0bfe492ca
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-23 21:04:30 +0500

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb+srv://Umidjon:Umidjon2005@cluster0.3mqkdgy.mongodb.net/Umidjon", {
+   await mongoose.connect("mongodb+srv://Umidjon:Umidjon2005@cluster0.3mqkdgy.mongodb.net/Umidjon", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,
     });
+
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
