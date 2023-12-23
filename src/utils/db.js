134a89250495495951f8c814cc3d85128ffb6c0d
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-23 12:59:24 +0500

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,8 +2,15 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("MongoDB connected successfully!");
   } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
     throw new Error("Connection failed!");
   }
 };
