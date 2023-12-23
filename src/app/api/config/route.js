135eb08a9bd24531c703e8e41fa9d6d491eb9a59
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-23 11:28:48 +0500

    updated status code syntax

--- src/app/api/config/route.js
+++ src/app/api/config/route.js
@@ -4,11 +4,11 @@ export const GET = async (req, res) => {
   if (req.method === 'GET') {
     try {
       const email = process.env.email || 'default@email.com';
-      res.status(200).json({ email });
+      res.json({ email },{status:200});
     } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.json({ error: 'Internal Server Error' },{status:500});
     }
   } else {
-    res.status(405).end(); // Return method not allowed for other methods
+    res.end(); // Return method not allowed for other methods
   }
 };
